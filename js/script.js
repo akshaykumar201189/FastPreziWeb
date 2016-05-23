76b8137ba83a209ad192f3ef986b27f744579077
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-05-24 00:02:10 +0530

    Added search, sort features and other formatting changes

--- js/script.js
+++ js/script.js
@@ -12,9 +12,16 @@ function getScrollTop() {
     return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 }
 
-function fetchPage(page) {
+function fetchPage(page, text, sort) {
+    var url = 'http://localhost:8080/presentations';
+    if(!text)
+        url += '?page_number=' + page + '&page_size=4';
+    else
+        url += '/search?title=' + text + '&page_number=' + page + '&page_size=4';
+    if(sort)
+        url += '&sort=true'
     $.ajax({
-        url: 'http://localhost:8080/presentations?page_number=' + page + '&page_size=4',
+        url: url,
         method: 'get',
         success: function (result) {
             var arrayLength = result.length;
@@ -22,17 +29,22 @@ function fetchPage(page) {
                 return;
             for (var i = 0; i < arrayLength; i++) {
                 var imageUrl=result[i].thumbnail;
-                $('#list').append('<li><div class="myimg"><img src=' + imageUrl + '></img></div><div class="myimgtitle">' + result[i].title + '</div><div class="myimgdetails">By <a href="' + result[i].creator.profileUrl + '" class="creatorProfileUrl">'+ result[i].creator.name  + '</a>' + ' on ' + result[i].createdAt + '</div></li>');
+                $('#list').append('<li><div class="myimg"><img height="480" width="480" src=' + imageUrl + '></img></div><div class="myimgtitle">' + result[i].title + '</div><div class="myimgdetails">By <a href="' + result[i].creator.profileUrl + '" class="creatorProfileUrl">'+ result[i].creator.name  + '</a>' + ' on ' + result[i].createdAt + '</div></li>');
             }
         }
     })
 }
 
 function addPage(page) {
-    fetchPage(page);
+    var searchText = $('#q').val();
+    if($('.check').prop("checked") == true){
+        fetchPage(page, searchText, true);
+    } else {
+        fetchPage(page, searchText, false);
+    }
 }
 
-let page = 0;
+let page = -1;
 
 addPage(++page);
 
@@ -40,3 +52,20 @@ window.onscroll = function() {
     if (getScrollTop() < getDocumentHeight() - window.innerHeight) return;
     addPage(++page);
 };
+
+
+$(document).ready(function(){
+    $(".q").keyup($.debounce( 250, function(e){
+        var val=$(e.target).val();
+        if(val){
+            $("#list").html("");
+            page = -1;
+            addPage(++page);
+        }
+    }));
+    $(".check").click($.debounce( 250, function(e) {
+        $('#list').html("");
+        page = -1;
+        addPage(++page);
+    }));
+})
